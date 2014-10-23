2cef9196b25d250ece6f3e8e4a698218dacb6fe6
ericak11 <kantor.erica@gmail.com>
2014-10-23 15:55:28 -0400

    initial commit on chat app demo

---
+++ index.js
@@ -0,0 +1,34 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
+
+var usernames = {};
+
+app.get('/', function(req, res){
+  res.sendfile('index.html');
+});
+
+http.listen(port, function(){
+  console.log('listening on *:' + port);
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    console.log('message: ' + msg);
+  });
+  socket.on('add user', function (username) {
+    // we store the username in the socket session for this client
+    socket.username = username;
+    // add the client's username to the global list
+    usernames[username] = username;
+    socket.broadcast.emit('user joined', {
+      username: socket.username,
+    });
+    console.log(usernames)
+  });
+});
+
+
+
