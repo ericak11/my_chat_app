9a9f8f94d8dcab07115f8261a764d94f342d4ee5
ericak11 <kantor.erica@gmail.com>
2014-10-23 18:06:44 -0400

    pushing

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 
-var app = require('express')();
-var http = require('http');
-var server = http.createServer(app);
-var io = require('socket.io')(server);
-// var port = process.env.PORT || 3000;
-var usernames = {};
+// var app = require('express')();
+// var http = require('http');
+// var server = http.createServer(app);
+// var io = require('socket.io')(server);
+// // var port = process.env.PORT || 3000;
+// var usernames = {};
 
 
-app.get('/', function(req, res){
-  res.sendfile('index.html');
-});
+// app.get('/', function(req, res){
+//   res.sendfile('index.html');
+// });
 
 
-// http.listen(port);
-var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
-var server_host = process.env.YOUR_HOST || '0.0.0.0';
-server.listen(server_port, server_host);
+// // http.listen(port);
+// var server_port = process.env.YOUR_PORT || process.env.PORT || 3000;
+// var server_host = process.env.YOUR_HOST || '0.0.0.0';
+// server.listen(server_port, server_host);
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-    // console.log('message: ' + msg);
-  });
-  socket.on('add user', function (username) {
-    // we store the username in the socket session for this client
-    socket.username = username;
-    // add the client's username to the global list
-    usernames[username] = username;
-    socket.broadcast.emit('user joined', {
-      username: socket.username,
-    });
-  });
-});
+// io.on('connection', function(socket){
+//   socket.on('chat message', function(msg){
+//     io.emit('chat message', msg);
+//     // console.log('message: ' + msg);
+//   });
+//   socket.on('add user', function (username) {
+//     // we store the username in the socket session for this client
+//     socket.username = username;
+//     // add the client's username to the global list
+//     usernames[username] = username;
+//     socket.broadcast.emit('user joined', {
+//       username: socket.username,
+//     });
+//   });
+// });
 
 
 
